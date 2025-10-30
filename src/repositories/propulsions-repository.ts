ea691ba6dc62a283333cbd62a4f5a3ed6e21b35f
mariocesar33 <mcgato33@hotmail.com>
2025-10-30 21:37:26 -0100

    Update propulsions-repository.ts

--- src/repositories/propulsions-repository.ts
+++ src/repositories/propulsions-repository.ts
@@ -12,6 +12,7 @@ export interface PropulsionsRepository {
   create(data: CreatePropulsionDTO): Promise<PropulsionDTO>
   save(propulsion: PropulsionDTO): Promise<PropulsionDTO>
   delete(id: string): Promise<void>
+  findById(id: string): Promise<PropulsionDTO | null>
   findByDescription(description: string): Promise<PropulsionDTO | null>
   getAllPropulsions(): Promise<PropulsionDTO[]>
 }
